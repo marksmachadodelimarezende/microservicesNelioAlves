41452a58d9895a1199e3e04c50e213922bcea6a8
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-04-08 10:09:14 -0300

    Hystrix fault tolerance. Include test of timeout in hr-worker, resource method findById.

--- hr-payroll/src/main/java/com/microservice/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/resources/PaymentResource.java
@@ -2,10 +2,13 @@ package com.microservice.hrpayroll.resources;
 
 import com.microservice.hrpayroll.entities.Payment;
 import com.microservice.hrpayroll.services.PaymentService;
-import org.apache.coyote.Response;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -18,10 +21,18 @@ public class PaymentResource {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         var response = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(response);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        var response = new Payment("Alternative", 1d, days);
+        return ResponseEntity.ok(response);
+    }
+
+
 }
 
--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResouce.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResouce.java
@@ -35,6 +35,14 @@ public class WorkerResouce {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        /*
+        try {
+            Thread.sleep(3000L); //3 segundos - Ribbon por default aguarda 1 segundo para fazer o balanceamento de carga.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         */
+        
         logger.info("PORT: " + env.getProperty("local.server.port"));
         var response = workerRepository.findById(id).get();
         return ResponseEntity.ok(response);
