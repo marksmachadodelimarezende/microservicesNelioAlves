26853eaa7bf96207f4a4dac6de6f2ec01308cd8d
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-04-08 11:21:00 -0300

    Project hr-api-gatway-zuul created

---
+++ hr-api-gatway-zuul/src/test/java/com/microserce/hrapigatwayzuul/HrApiGatwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserce.hrapigatwayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatwayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
