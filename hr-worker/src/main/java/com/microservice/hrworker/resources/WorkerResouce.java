5e24571ae45bb2551df373ef42e0d79088b25a83
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-06-18 11:25:01 -0300

    Actuator for runtime config test

--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResouce.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResouce.java
@@ -1,17 +1,23 @@
 package com.microservice.hrworker.resources;
 
-import com.microservice.hrworker.entities.Worker;
-import com.microservice.hrworker.repositories.WorkerRepository;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.microservice.hrworker.entities.Worker;
+import com.microservice.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value="/workers")
 public class WorkerResouce {
