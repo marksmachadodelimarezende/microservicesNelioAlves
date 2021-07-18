863757274823ae87893374d3b0d89b289bba7c73
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2021-07-18 19:08:29 -0300

    Adjust version Maven downgrade to 4.5.9-SNAPSHOT

--- hr-oauth/src/test/java/com/microservice/hroauth/HrOauthApplicationTests.java
+++ hr-oauth/src/test/java/com/microservice/hroauth/HrOauthApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HrOauthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
